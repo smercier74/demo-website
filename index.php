e8190b0f307dece3c7fbb4cf47731a49124d0227
Mercier <6613931+smercier74@users.noreply.github.com>
2021-01-05 16:25:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo phpinfo();
+//echo phpinfo();
+try {
+    $user='DBUser';
+    $pass='DBPwd';
+    $db_host='mysql.trainee-1';
+    $db_name='trainee-db';
+    $dbh = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $user, $pass);
+    foreach($dbh->query('SELECT * from users') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
