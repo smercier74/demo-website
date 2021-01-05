fd329c0b32c6d458826a44f080d0ec7337026f02
Mercier <6613931+smercier74@users.noreply.github.com>
2021-01-05 16:38:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 //echo phpinfo();
 try {
+    //Read DB settings
     $user=getenv("MYSQL_USER");//'DBUser';
     $pass=getenv("MYSQL_PASSWORD");//'DBPwd';
     $db_host=getenv("DB_HOST");//'mysql.trainee-1';
     $db_name=getenv("MYSQL_DATABASE");//'trainee-db';
-    //Read data from the DB
+    //Open DB cnx
     $dbh = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $user, $pass);
+    //Read data from the DB
     foreach($dbh->query('SELECT * from users') as $row) {
         //Display each row
         print_r($row);
     }
+    //Close DB cnx
     $dbh = null;
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
