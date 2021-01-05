063dfb635c91078033f04ac00b2e6d11671fb3a7
Mercier <6613931+smercier74@users.noreply.github.com>
2021-01-05 16:44:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ try {
     //Read DB settings
     $user=getenv("MYSQL_USER");//'DBUser';
     $pass=getenv("MYSQL_PASSWORD");//'DBPwd';
-    $db_host=getenv("DB_HOST1");//'mysql.trainee-1';
+    $db_host=getenv("DB_HOST");//'mysql.trainee-1';
     $db_name=getenv("MYSQL_DATABASE");//'trainee-db';
     //Open DB cnx
     $dbh = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $user, $pass);
